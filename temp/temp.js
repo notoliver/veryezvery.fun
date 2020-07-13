84542a3b9adb6e008d7380752bad80a577fbea55
notoliver <47568829+notoliver@users.noreply.github.com>
2020-07-13 18:15:32 -0400

    ok

--- temp/temp.js
+++ temp/temp.js
@@ -1,6 +1,4 @@
-function chat(){
-    var input = document.getElementById('myText').value;
-    document.getElementById('myText').value='';
+function getTime(){
     var d = new Date();
     var currentHours = d.getHours();
     currentHours = ("0" + currentHours).slice(-2);
@@ -9,9 +7,51 @@ function chat(){
     var currentSeconds = d.getSeconds();
     currentSeconds = ("0" + currentSeconds).slice(-2);
     var time = currentHours+':'+currentMinutes+':'+currentSeconds;
-    document.getElementById('chat').innerHTML+='<p>'+time+' '+input+'<\/p>';
+    return time;
+}
+
+var userInput = [];
+function chat(){
+    var input = document.getElementById('myText').value;
+    if(input==''){
+        return;
+    }
+    userInput.push(input);
+    document.getElementById('myText').value='';
+    document.getElementById('chat').innerHTML+='<p>'+getTime()+' me: '+input+'<\/p>';
+}
+function joined(){
+    document.getElementById('chat').innerHTML+='<p>'+getTime()+' <i>i joined the chatroom<\/i><\/p>';
+}
+
+var lines = ['hello','hi','what is this','lol','why','who','i mean','ok','umm','awkward','stop please','ok bye','lmao'];
+var names = ['john','mary','matt','liam','sophia','amelia','logan','mason','lucas','elijah','james','emma','evelyn'];
+var lastUsedLine =''
+
+function crowd(){
+    var decision = Math.floor(Math.random() * 8);
+    var input;
+    if(decision==0 && userInput.length!=0){
+        input = "\""+userInput[Math.floor(Math.random() * userInput.length)]+"\" ok...?";
+    }
+    else{
+        input = lines[Math.floor(Math.random() * lines.length)];
+        while(input==lastUsedLine){
+            input = lines[Math.floor(Math.random() * lines.length)];
+        }
+    }
+    lastUsedLine=input;
+    var name = names[Math.floor(Math.random() * names.length)];  
+    document.getElementById('chat').innerHTML+='<p>'+getTime()+' '+name+': '+input+'<\/p>';
+}
+function myFunction() {
+    crowd();
+    var time = 100*(Math.floor(Math.random() * (120 - 40 + 1)) + 40);
+    setTimeout(myFunction, time);
 }
 
 $(function() {
     $('.window').draggable({ handle:'.header'});
+    setTimeout(joined,120)
+    setTimeout(myFunction,100*(Math.floor(Math.random() * (70 - 30 + 1)) + 30));
 });
