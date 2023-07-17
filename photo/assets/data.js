2e4c8999c0eb27a507beb43a8c533b3373a1c42d
notoliver <oliver.qiu@hotmail.com>
2023-07-17 14:31:53 -0700

    pics

---
+++ photo/assets/data.js
@@ -0,0 +1,244 @@
+var home = [
+    {
+        module: 'responsivegallery',
+        id: 'collective',
+        targetHeight: 700,
+        offset: 0,
+        filenames: [
+            {
+                image: '1.jpg',
+                x: '1',
+                y: '1'
+            },
+            {
+                image: '2.jpg',
+                x: '1',
+                y: '1'
+            },
+            {
+                image: '3.jpg',
+                x: '180',
+                y: '178'
+            },
+            {
+                image: '4.jpg',
+                x: '1',
+                y: '1'
+            },
+            {
+                image: '5.jpg',
+                x: '1',
+                y: '1'
+            },
+            {
+                image: '6.jpg',
+                x: '4',
+                y: '3'
+            },
+            {
+                image: '7.jpg',
+                x: '4',
+                y: '3'
+            },
+            {
+                image: '8.jpg',
+                x: '1',
+                y: '1'
+            },
+            {
+                image: '9.jpg',
+                x: '1',
+                y: '1'
+            },
+            {
+                image: '10.jpg',
+                x: '125',
+                y: '130'
+            }
+        ]
+    },
+    {
+        module: 'spacer',
+        height: '10px'
+    }
+]
+
+var minsuh = [
+    {
+        module: 'responsivegallery',
+        id: 'minsuhchung',
+        targetHeight: 600,
+        offset: 0,
+        filenames: [
+            {
+                image: '1.jpg',
+                x: '4',
+                y: '3'
+            },
+            {
+                image: '2.jpg',
+                x: '113',
+                y: '120'
+            },
+            {
+                image: '3.jpg',
+                x: '4',
+                y: '3'
+            },
+            {
+                image: '4.jpg',
+                x: '4',
+                y: '3'
+            },
+            {
+                image: '5.jpg',
+                x: '3',
+                y: '4'
+            },
+            {
+                image: '6.jpg',
+                x: '1',
+                y: '1'
+            },
+            {
+                image: '7.jpg',
+                x: '1',
+                y: '1'
+            },
+            {
+                image: '8.jpg',
+                x: '1',
+                y: '1'
+            },
+            {
+                image: '9.jpg',
+                x: '180',
+                y: '144'
+            },
+            {
+                image: '10.jpg',
+                x: '180',
+                y: '137'
+            },
+            {
+                image: '11.jpg',
+                x: '3',
+                y: '4'
+            },
+            {
+                image: '12.jpg',
+                x: '4',
+                y: '3'
+            },
+            {
+                image: '13.jpg',
+                x: '180',
+                y: '143'
+            },
+            {
+                image: '14.jpg',
+                x: '1',
+                y: '1'
+            },
+            {
+                image: '15.jpg',
+                x: '4',
+                y: '3'
+            }
+        ]
+    },
+    {
+        module: 'spacer',
+        height: '10px'
+    }
+]
+
+var oliver = [
+    {
+        module: 'responsivegallery',
+        id: 'oliverqiu',
+        targetHeight: 600,
+        offset: 0,
+        filenames: [
+            {
+                image: '1.jpg',
+                x: '1',
+                y: '1'
+            },
+            {
+                image: '2.jpg',
+                x: '1',
+                y: '1'
+            },
+            {
+                image: '3.jpg',
+                x: '1',
+                y: '1'
+            },
+            {
+                image: '4.jpg',
+                x: '1',
+                y: '1'
+            },
+            {
+                image: '5.jpg',
+                x: '1',
+                y: '1'
+            },
+            {
+                image: '6.jpg',
+                x: '1',
+                y: '1'
+            },
+            {
+                image: '7.jpg',
+                x: '1',
+                y: '1'
+            },
+            {
+                image: '8.jpg',
+                x: '1',
+                y: '1'
+            },
+            {
+                image: '9.jpg',
+                x: '1',
+                y: '1'
+            },
+            {
+                image: '10.jpg',
+                x: '1',
+                y: '1'
+            },
+            {
+                image: '11.jpg',
+                x: '1',
+                y: '1'
+            },
+            {
+                image: '12.jpg',
+                x: '1',
+                y: '1'
+            },
+            {
+                image: '13.jpg',
+                x: '1',
+                y: '1'
+            },
+            {
+                image: '14.jpg',
+                x: '1',
+                y: '1'
+            },
+            {
+                image: '15.jpg',
+                x: '1',
+                y: '1'
+            }
+        ]
+    },
+    {
+        module: 'spacer',
+        height: '10px'
+    }
+]
---
+++ photo/assets/scripts/sidemenu.js
@@ -0,0 +1,55 @@
+var text='';
+
+var active = document.getElementById("navmenus").getAttribute("active");
+console.log(active);
+
+//var pages = ["home","lisbon", "uk","bio","geneva"];
+
+/*
+<div class='sidemenu'>
+    <div style="padding-bottom:20px;">
+        <a href="/photo" style="font-size:20px;" pp="pp">photography</a>
+    </div>
+    
+        <a href="/photo/minsuhchung" >minsuh chung</a>
+        <br>
+        <a href="/photo/oliverqiu" >oliver qiu</a>
+    
+</div>
+
+<div class='mobilemenu'>
+
+    <div>
+        <a href="/photo">photography</a>
+        <span style="float:right;" onclick="openNavModal()">
+            menu 
+        </span>
+    </div>
+
+    <div class="navmodal" id="myNavModal">
+        <span class="close cursor" onclick="closeNavModal()">&times;</span>
+        <div class="navmodal-content">
+            <a href="/photo" pp="pp">photography</a>
+    <br>
+    <a href="/photo/minsuhchung">minsuh chung</a>
+    <br>
+    <a href="/photo/oliverqiu">oliver qiu</a>
+        </div>
+    </div>
+
+</div>
+*/
+
+
+text+='<div class=\'sidemenu\'>\r\n    <div style=\"padding-bottom:20px;\">\r\n        <a href=\"\/photo\" style=\"font-size:20px;\" pp=\"pp\">photography<\/a>\r\n    <\/div>\r\n    \r\n        <a href=\"\/photo\/minsuhchung\" >minsuh chung<\/a>\r\n    \r\n <br>       <a href=\"\/photo\/oliverqiu\" >oliver qiu<\/a>\r\n    \r\n<\/div>\r\n\r\n<div class=\'mobilemenu\'>\r\n\r\n    <div>\r\n        <a href=\"\/photo\">photography<\/a>\r\n        <span style=\"float:right;\" onclick=\"openNavModal()\">\r\n            menu \r\n        <\/span>\r\n    <\/div>\r\n\r\n    <div class=\"navmodal\" id=\"myNavModal\">\r\n        <span class=\"close cursor\" onclick=\"closeNavModal()\">&times;<\/span>\r\n        <div class=\"navmodal-content\">\r\n            <a href=\"\/photo\" pp=\"pp\">photography<\/a>\r\n    <br>\r\n    <a href=\"\/photo\/minsuhchung\">minsuh chung<\/a>\r\n    <br>\r\n    <a href=\"\/photo\/oliverqiu\">oliver qiu<\/a>\r\n        <\/div>\r\n    <\/div>\r\n\r\n<\/div>'
+
+var text2=text;
+
+if(active!=="home"){
+    var results = getIndicesOf(active, text,true);
+    var firstoccurance=results[0]+active.length+1;
+    var secondoccurance=results[1]+active.length+1;
+    text2 = text.slice(0, firstoccurance) + " class=\"active\" " + text.slice(firstoccurance,secondoccurance)+ " class=\"active\" " +text.slice(secondoccurance);
+}
+
+document.getElementsByClassName('navmenus')[0].innerHTML = text2;
