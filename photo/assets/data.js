a06b4cd9ea76e429ab22231e57e8e0ccab9c1cee
notoliver <oliver.qiu@hotmail.com>
2023-07-17 22:04:59 -0700

    asdf

--- photo/assets/data.js
+++ photo/assets/data.js
@@ -8,52 +8,62 @@ var home = [
             {
                 image: '1.jpg',
                 x: '1',
-                y: '1'
+                y: '1',
+                caption:'Los Angeles, CA | Sigma FP'
             },
             {
                 image: '2.jpg',
                 x: '1',
-                y: '1'
+                y: '1',
+                caption:'Youngwol, South Korea | Sigma FP'
             },
             {
                 image: '3.jpg',
                 x: '180',
-                y: '178'
+                y: '178',
+                caption:'Strasbourg, France | Canon PowerShot S90'
             },
             {
                 image: '4.jpg',
                 x: '1',
-                y: '1'
+                y: '1',
+                caption:'Youngwol, South Korea | Sigma FP'
             },
             {
                 image: '5.jpg',
                 x: '1',
-                y: '1'
+                y: '1',
+                caption:'Gainesville, FL | Sigma FP'
             },
             {
                 image: '6.jpg',
                 x: '4',
-                y: '3'
+                y: '3',
+                caption:'Youngwol, South Korea | Panasonic DMC-ZS50'
             },
             {
                 image: '7.jpg',
                 x: '4',
-                y: '3'
+                y: '3',
+                caption:'Frankfurt, Germany | Canon PowerShot S90'
             },
             {
                 image: '8.jpg',
                 x: '1',
-                y: '1'
+                y: '1',
+                caption:'Chicago, IL | Sigma FP'
             },
             {
                 image: '9.jpg',
                 x: '1',
-                y: '1'
+                y: '1',
+                caption:'Chicago, IL | Sigma FP'
             },
             {
                 image: '10.jpg',
                 x: '125',
-                y: '130'
+                y: '130',
+                caption:'Colmar, France | Canon PowerShot S90'
             }
         ]
     },
@@ -73,77 +83,92 @@ var minsuh = [
             {
                 image: '1.jpg',
                 x: '4',
-                y: '3'
+                y: '3',
+                caption: 'Presence is yet! | Frankfurt, Germany | Panasonic DMC-ZS50'
             },
             {
                 image: '2.jpg',
                 x: '113',
-                y: '120'
+                y: '120',
+                caption: 'Colmar, France | Canon PowerShot S90'
             },
             {
                 image: '3.jpg',
                 x: '4',
-                y: '3'
+                y: '3',
+                caption: 'Doll house | Seoul, South Korea | Canon PowerShot S90'
             },
             {
                 image: '4.jpg',
                 x: '4',
-                y: '3'
+                y: '3',
+                caption: 'Seoul, South Korea | Canon PowerShot S90'
             },
             {
                 image: '5.jpg',
                 x: '3',
-                y: '4'
+                y: '4',
+                caption: 'Tree on Tree | Frankfurt, Germany | Canon PowerShot S90'
             },
             {
                 image: '6.jpg',
                 x: '1',
-                y: '1'
+                y: '1',
+                caption: 'Food poisoning | Chicago, Illinois | Sigma FP'
             },
             {
                 image: '7.jpg',
                 x: '1',
-                y: '1'
+                y: '1',
+                caption: 'Carpet | Youngwol, South Korea | Sigma FP'
             },
             {
                 image: '8.jpg',
                 x: '1',
-                y: '1'
+                y: '1',
+                caption: 'Collection | Youngwol, South Korea | Sigma FP'
             },
             {
                 image: '9.jpg',
                 x: '180',
-                y: '144'
+                y: '144',
+                caption: 'Strasbourg, France | Panasonic DMC-ZS50'
             },
             {
                 image: '10.jpg',
                 x: '180',
-                y: '137'
+                y: '137',
+                caption: 'Great Stuff | Youngwol, South Korea | Canon PowerShot S90'
             },
             {
                 image: '11.jpg',
                 x: '3',
-                y: '4'
+                y: '4',
+                caption: 'Colmar, France | Canon PowerShot S90'
             },
             {
                 image: '12.jpg',
                 x: '4',
-                y: '3'
+                y: '3',
+                caption: 'Picnic 1 | Strasbourg, France | Panasonic DMC-ZS50'
             },
             {
                 image: '13.jpg',
                 x: '180',
-                y: '143'
+                y: '143',
+                caption: 'Picnic 2 | Strasbourg, France | Panasonic DMC-ZS50'
             },
             {
                 image: '14.jpg',
                 x: '1',
-                y: '1'
+                y: '1',
+                caption: 'Youngwol, South Korea | Sigma FP'
             },
             {
                 image: '15.jpg',
                 x: '4',
-                y: '3'
+                y: '3',
+                caption: '<a href=\'https://open.spotify.com/track/6Fjd5gR9OuFHrlxgY50O8p?si=aad6cef9891d4f40\' target=\'_blank\'>Chicago, Illinois</a> | Panasonic DMC-ZS50'
             }
         ]
     },
@@ -163,77 +188,92 @@ var oliver = [
             {
                 image: '1.jpg',
                 x: '1',
-                y: '1'
+                y: '1',
+                caption: ''
             },
             {
                 image: '2.jpg',
                 x: '1',
-                y: '1'
+                y: '1',
+                caption: ''
             },
             {
                 image: '3.jpg',
                 x: '1',
-                y: '1'
+                y: '1',
+                caption: ''
             },
             {
                 image: '4.jpg',
                 x: '1',
-                y: '1'
+                y: '1',
+                caption: ''
             },
             {
                 image: '5.jpg',
                 x: '1',
-                y: '1'
+                y: '1',
+                caption: ''
             },
             {
                 image: '6.jpg',
                 x: '1',
-                y: '1'
+                y: '1',
+                caption: ''
             },
             {
                 image: '7.jpg',
                 x: '1',
-                y: '1'
+                y: '1',
+                caption: ''
             },
             {
                 image: '8.jpg',
                 x: '1',
-                y: '1'
+                y: '1',
+                caption: ''
             },
             {
                 image: '9.jpg',
                 x: '1',
-                y: '1'
+                y: '1',
+                caption: ''
             },
             {
                 image: '10.jpg',
                 x: '1',
-                y: '1'
+                y: '1',
+                caption: ''
             },
             {
                 image: '11.jpg',
                 x: '1',
-                y: '1'
+                y: '1',
+                caption: ''
             },
             {
                 image: '12.jpg',
                 x: '1',
-                y: '1'
+                y: '1',
+                caption: ''
             },
             {
                 image: '13.jpg',
                 x: '1',
-                y: '1'
+                y: '1',
+                caption: ''
             },
             {
                 image: '14.jpg',
                 x: '1',
-                y: '1'
+                y: '1',
+                caption: ''
             },
             {
                 image: '15.jpg',
                 x: '1',
-                y: '1'
+                y: '1',
+                caption: ''
             }
         ]
     },
@@ -251,6 +291,7 @@ var about = [
     },
     {
         module: 'description',
-        text: 'pictures taken by <a href=\'https://www.minsuhchung.com/\' target="_blank" style="color:black;">minsuh chung</a> and <a href=\'https://oliverqiu.cool/\' target="_blank" style="color:black;">oliver qiu</a>'
+        text: 'pictures taken by <a href=\'https://www.minsuhchung.com/\' target="_blank" style="color:black;">minsuh chung</a> and <a href=\'https://oliverqiu.cool/\' target="_blank" style="color:black;">oliver qiu</a>',
+        inline: 'style=\'text-align:left;\''
     }
 ]
